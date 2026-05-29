da170007cb50ac9130366f9a7c6c36a56db5b1c3
Abbas kermani <abbas.kermni@gmail.com>
2026-05-29 21:45:32 +0330

    dockeriaz project backend and  frontend and cheng db to postgers

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
 from api import models as api_models
 
 # Define a custom serializer that inherits from TokenObtainPairSerializer
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB", "mydb"),
+        'USER': os.getenv("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "postgres"),
+        'HOST': os.getenv("POSTGRES_HOST", "db"),
+        'PORT': 5432,
     }
 }
 
